3288101614dbe2b3ec92c0dc04f599f5acc007aa
leodelafiori <31015822+leodelafiori@users.noreply.github.com>
2018-09-07 09:41:44 -0300

    combat

--- Assets/Scripts/Player/Player_Human_Combat.cs
+++ Assets/Scripts/Player/Player_Human_Combat.cs
@@ -5,57 +5,94 @@ using UnityEngine;
 public class Player_Human_Combat : MonoBehaviour {
 
     #region Declaring variables
- //Main variables
-    private float timeBtwAttacks = 1;
-    private float resetTimeBtwAttacks = 1;
+    //Variables for the reset time on combos
+    [SerializeField] private float timeBtwAttacks;
     [SerializeField] private LayerMask whatIsEnemies;
     [SerializeField] private Transform attackPos;
     [SerializeField]private float attackRange = 0.5f;
     [SerializeField] private int damage = 20;
     private Animator anim;
     public static bool isAttacking = false;
-
+    private int comboIndex;
+    private int heavyComboIndex;
     private float timerAttackAnimation = 1;
     [SerializeField] private float maxTimerAttackAnimation = 1;
     #endregion
     #region Awake
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();    
+
     }
     #endregion
 
     #region Update Function
     void Update () {
-        if (timeBtwAttacks <= 0 && Input.GetMouseButton(0) && Player_Human_Movement.isMovable)
+
+        //Setting the damage and trigger animations
+        if (timeBtwAttacks <= 0.1 && Input.GetMouseButton(1))
         {
             isAttacking = true;
-            Collider2D[] enemyToDamage = Physics2D.OverlapCircleAll(attackPos.position,attackRange,whatIsEnemies);
-            anim.SetTrigger("LightAttack1");
-            for(int i = 0; i < enemyToDamage.Length; i++)
-            {
-                enemyToDamage[i].GetComponent<Enemy1>().TakeDamage(damage);
-            }
-            timeBtwAttacks = resetTimeBtwAttacks;
-        } else
+            Player_Human_Movement.isMovable = false;
+            comboIndex++;
+            anim.SetInteger("ComboIndex", comboIndex);
+            anim.SetTrigger("HeavyAttack");
+            timeBtwAttacks = anim.GetCurrentAnimatorStateInfo(0).length;
+
+        }
+        if (timeBtwAttacks <= 0.1 && Input.GetMouseButton(0))
+        {
+            isAttacking = true;
+            Player_Human_Movement.isMovable = false;
+            comboIndex++;
+            anim.SetInteger("ComboIndex", comboIndex);
+            anim.SetTrigger("LightAttack");          
+            timeBtwAttacks = anim.GetCurrentAnimatorStateInfo(0).length;
+
+        } else if (timeBtwAttacks <= 0)
+        {
+            ResetComboIndex();
+            Player_Human_Movement.isMovable = true;
+            isAttacking = false;
+        } 
+        else
         {
             timeBtwAttacks -= Time.deltaTime;
         }
-        if (isAttacking)
-        {
-
-            timerAttackAnimation -= Time.deltaTime;
-
-            //test -= Time.deltaTime;
-            if (timerAttackAnimation <= 0)
-            {
-                timerAttackAnimation = maxTimerAttackAnimation;
-                isAttacking = false;
-            }
-        }
+       
 	}
     #endregion
 
+    #region DamageEnemies(int damageTimer) private function and minor functions to now allow player movement with the animation frames
+    private void DamageEnemies() //To be used in the animation frame events
+    {
+        Collider2D[] enemyToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+        for (int i = 0; i < enemyToDamage.Length; i++)
+        {
+            enemyToDamage[i].GetComponent<Enemy1>().TakeDamage(damage);
+        }
+        transform.Translate(0.05f, 0, 0);
+    }
+    private void Attacking()
+    {
+        Player_Human_Movement.isMovable = false;
+    }
+
+    private void NotAttacking()
+    {
+        Player_Human_Movement.isMovable = false;
+    }
+    #endregion
+
+    #region ResetComboIndex private
+    private void ResetComboIndex()
+    {
+        heavyComboIndex = 0;
+        anim.SetInteger("HeavyComboIndex", 0);
+        comboIndex = 0;
+        anim.SetInteger("ComboIndex", 0);
+    }
+    #endregion
     #region onDrawGizmosSelected (visualize the player attack range
     private void OnDrawGizmosSelected()
     {
